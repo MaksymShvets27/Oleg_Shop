f70e1edf90244bb5b798b358533acb2c48d99c8f
MaksymShvets27 <MaksymShvets27@users.noreply.github.com>
2023-07-03 20:04:32 +0000

    deploy: 76edda30d6654a7adae4aea92f3701380ab60231

--- assets/index-08b1bdd9.js
+++ assets/index-08b1bdd9.js
@@ -93,7 +93,7 @@ Error generating stack: `+o.message+`
 `,kx=fe(yv)`
   color: black;
   font-family: Poppins;
-  font-size: 16px;
+  font-size: 10px;
   font-weight: 600;
   line-height: 22px;
   letter-spacing: 0em;
